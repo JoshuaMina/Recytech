7d3f2c512e3e187388c71e8c4f826c29ba1fc30f
JoshuaMina <joshua.minacambronero@gmail.com>
2025-01-14 09:43:18 +0800

    authentication progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,57 @@ import { supabase } from './createClient';
 
 const App = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [formData,setFormData] = useState({
+    firstName: '', lastName: '', email: '', password: ''
+  })
 
   const toggleForm = () => {
     setIsLogin(!isLogin);
   };
 
   
+  function handleChange(event) {
+    setFormData(prevFormData=>{
+      return {
+        ...prevFormData,
+        [event.target.name]:event.target.value
+      }
+      
+
+
+    })
+  }
+
+  async function handleSubmit(event){
+    event.preventDefault(); 
+    try {
+      const { data, error } = await supabase.auth.signUp(
+        {
+          email: formData.email,
+          password: formData.password,
+          options: {
+            data: {
+              first_name: formData.firstName,
+              age: formData.lastName,
+            }
+          }
+        }
+      )
+
+      alert('Confirmation already sent to your email. Kindly view the email for verification :)')
+    } catch (error) {
+      alert (error)
+    }
+    
+  }
+
+  console.log(formData)
+
+
+
+
+
+
   const signInWithGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
@@ -23,6 +68,10 @@ const App = () => {
     });
     if (error) console.error("Facebook Sign-In Error:", error);
   };
+
+
+
+
   return (
     <div className="app-wrapper">
      
@@ -36,24 +85,36 @@ const App = () => {
       <div className="form-container">
         <div className={`form-wrapper ${isLogin ? "login-mode" : "signup-mode"}`}>
           {/* Login Form */}
-          <div className={`form login-form ${isLogin ? "active" : ""}`}>
+          <div className={`form login-form ${isLogin ? "active" : ""}`} > 
             <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
             <div className="input-group">
-              <input type="text" placeholder="Username or Email" />
+              <input type="text" placeholder="Email" onChange={handleChange}/>
               <i className="bx bx-user"></i>
             </div>
             <div className="input-group">
-              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Password" onChange={handleChange}/>
               <i className="bx bx-lock-alt"></i>
             </div>
+            
             <div className="input-group">
-              <button className="btn">Login</button>
+              <button type= 'submit' className="btn">Login</button>
             </div>
-            <div className="input-group social">
-              <button className="kl social"><img src="/src/assets/img/google.png" alt="google Logo" className="icon" />  Login with Google</button>
-            </div>
-            <div className="input-group social">
-              <button className="kl social"><img src="/src/assets/img/google.png" alt="google Logo" className="icon" />  Login with Google</button>
+
+            </form>
+            
+            <p>
+              
+              <a href="#" onClick={toggleForm}>
+              Forget Password?
+              </a>
+            </p>
+            <p>
+              or
+            </p>
+            <div className="input-group flex-container">
+              <button className="kl "><img src="/src/assets/img/google.png" alt="google Logo" className="icon" />Google Login</button>
+              <button className="kl "><img src="/src/assets/img/fb.png" alt="google Logo" className="icon" />Facebook Login</button>
             </div>
             <p>
               Don't have an account?{" "}
@@ -67,24 +128,35 @@ const App = () => {
           {/* Signup Form */}
           <div className={`form signup-form ${!isLogin ? "active" : ""}`}>
             <h2>Sign Up</h2>
-            <div className="input-group">
-              <input type="text" placeholder="Firstname" />
-              <i className="bx bx-user"></i>
-            </div>
-            <div className="input-group">
-              <input type="text" placeholder="Lastname" />
-              <i className="bx bx-user"></i>
-            </div>
-            <div className="input-group">
-              <input type="email" placeholder="Email" />
-              <i className="bx bx-envelope"></i>
-            </div>
-            <div className="input-group">
-              <input type="password" placeholder="Password" />
-              <i className="bx bx-lock-alt"></i>
-            </div>
-            <div className="input-group">
-              <button className="btn">Register</button>
+
+            <form onSubmit={handleSubmit}>
+              <div className="input-group">
+                <input type="text" placeholder="Firstname" name='firstName' onChange={handleChange}/>
+                <i className="bx bx-user"></i>
+              </div>
+              <div className="input-group">
+                <input type="text" placeholder="Lastname"  name='lastName'onChange={handleChange}/>
+                <i className="bx bx-user"></i>
+              </div>
+              <div className="input-group">
+                <input type="email" placeholder="Email"  name='email'onChange={handleChange}/>
+                <i className="bx bx-envelope"></i>
+              </div>
+              <div className="input-group">
+                <input type="password" placeholder="Password"  name='password' onChange={handleChange}/>
+                <i className="bx bx-lock-alt"></i>
+              </div>
+              <div className="input-group">
+                <button type= 'submit' className="btn">Register</button>
+              </div>
+            </form>
+            
+            <p>
+              or
+            </p>
+            <div className="input-group flex-container">
+              <button className="kl "><img src="/src/assets/img/google.png" alt="google Logo" className="icon" />Use Google</button>
+              <button className="kl "><img src="/src/assets/img/fb.png" alt="google Logo" className="icon" />Use Facebook</button>
             </div>
             <p>
               Already have an account?{" "}
--- src/createClient.js
+++ src/createClient.js
@@ -1,5 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
 
-export const supabase= createClient(
-    "https://kqajraddatqbgrrmlksn.supabase.co", 
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxYWpyYWRkYXRxYmdycm1sa3NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0MDIwOTYsImV4cCI6MjA1MTk3ODA5Nn0.34fOiINEOLKXcAQ0Cvgfh8q1DxVpO-eQ5Aye3sl5DxQ")
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://kqajraddatqbgrrmlksn.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxYWpyYWRkYXRxYmdycm1sa3NuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNjQwMjA5NiwiZXhwIjoyMDUxOTc4MDk2fQ.aa9H6fZrsFTQoxwa-TLvKbUvDrQ8AsWxPQ1y3O4qnyk'
+export  const supabase = createClient(supabaseUrl, supabaseKey) 
